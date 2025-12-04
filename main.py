6775af5db1780ff25d554ffc7f4967e019bbe01b
imrajankumar95 <imrajankumar95@gmail.com>
2025-12-04 01:16:03 -0500

    B: player, circleshape added for player

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import *
 from logger import log_state
+from player import Player
 
 def main():
     
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
@@ -16,7 +19,12 @@ def main():
                 return
             
         screen.fill("black")
+        player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS, 0)
+        player.draw(screen)
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
+        #print(dt)
 
 
 if __name__ == "__main__":
